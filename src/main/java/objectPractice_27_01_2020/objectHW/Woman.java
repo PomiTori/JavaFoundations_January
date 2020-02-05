61425e11d049c89651e2d34f485849445e16e11b
Юленька <julia_s_94@mail.ru>
2020-02-05 23:07:10 +0300

    commit equals

--- src/main/java/objectPractice_27_01_2020/objectHW/Woman.java
+++ src/main/java/objectPractice_27_01_2020/objectHW/Woman.java
@@ -1,5 +1,7 @@
 package objectPractice_27_01_2020.objectHW;
 
+import java.util.Objects;
+
 public class Woman {
 
     private String name;
@@ -17,10 +19,23 @@ public class Woman {
         return age;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-       if (this == obj) return true;
-       return false;
+//    @Override
+//    public boolean equals(Object obj) {
+//       if (this == obj) return true;
+//       return false;
+//    }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Woman woman = (Woman) o;
+//        if ((age - woman.age) == 0) {
+//            return true;}
+//            else return false;
+
+        return Math.abs(woman.age - age) < 0.000001 &&
+                (woman.name == name) &&
+                (woman.isCanSwim == isCanSwim);
     }
 
     @Override
